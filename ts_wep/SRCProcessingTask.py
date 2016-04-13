715010073d3d8843f9d6f1d9c92335bcf90715a5
J. Matt Peterson <jmatt@jmatt.org>
2016-04-13 16:23:31 -0700

    Add DateMinuteParameter. Use output objects. Update output in run method.
    	modified:   ts_wep/CWFSProcessingTask.py
    	modified:   ts_wep/CWFSTask.py
    	modified:   ts_wep/INSTSignatureRemovalTask.py
    	modified:   ts_wep/SRCProcessingTask.py
    	modified:   ts_wep/SRCSelectionTask.py

--- ts_wep/SRCProcessingTask.py
+++ ts_wep/SRCProcessingTask.py
@@ -1,13 +1,22 @@
+from datetime import datetime
+
 import luigi
+
 from SRCSelectionTask import SRCSelectionTask
+
 
 class SRCProcessingTask(luigi.Task):
 
+    date = luigi.DateMinuteParameter(default=datetime.now())
+
     def output(self):
-        return 1
+        return luigi.LocalTarget(
+            self.date.strftime("data/srcproccessing_%Y%m%d%H%M"))
 
     def requires(self):
         return SRCSelectionTask()
 
     def run(self):
+        with self.output().open('w') as f:
+            f.write(str(1))
         print "Running Processing Task"
